ebe16c36504ac6042b7d047647aa936cd8e95242
Nil <nilriera2000@gmail.com>
2017-05-12 22:29:16 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -4,6 +4,9 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import binarytodecimal
+ 
+hola= ""
+
 if __name__ == "__main__":
 # Create an PyQT5 application object.
     a = QApplication(sys.argv)
@@ -12,32 +15,42 @@ if __name__ == "__main__":
     w = QWidget()
 
     # Set window size.
-    w.resize(320, 240)
+    w.resize(420, 300)
 
     # Set window title
     w.setWindowTitle("GUI!")
 
-    btn = QPushButton('Hello World!', w)
+    btn = QPushButton('EXIT', w)
     btn.setToolTip('Click to quit!')
     btn.clicked.connect(exit)
     btn.resize(btn.sizeHint())
     btn.move(100, 100)
 
      # Create textbox
-    textbox1 = QLineEdit(w)
-    textbox1.move(20, 20)
-    textbox1.resize(280,40)
-
-    textbox1.textChanged.connect(binarytodecimal.decimal)
     
-    textbox2 = QLineEdit(w)
-    textbox2.move(20, 20)
-    textbox2.resize(280,40)
+    def decimaltobinary(inputdecimal):
+        decimal_value = str(binarytodecimal.decimal(inputdecimal)) #32
+        print(decimal_value)
+        textbox1.setText(decimal_value)
+    
+    textbox = QLineEdit(w)
+    textbox.setToolTip('Decimal')
+    textbox.move(20, 20)
+    textbox.resize(280,40)
+    textbox.textChanged.connect(decimaltobinary)
 
-    textbox2.textChanged.connect(binarytodecimal.binary)
-
+    def binarytodecimal1(inputbinary):
+        binary_value = str(binarytodecimal.binary(inputbinary))
+        print(binary_value)
+        textbox.setText(binary_value)
+    
+    
+    textbox1 = QLineEdit(w)
+    textbox1.setToolTip('Binary')
+    textbox1.move(20, 200)
+    textbox1.resize(280,40)
+    textbox1.textChanged.connect(binarytodecimal1) 
 
     w.show()
 
     sys.exit(a.exec_())
-
